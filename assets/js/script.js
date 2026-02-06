6fc88213ea310abaec1c0219a0958323921d4d37
Adil Israr <adilisrar572@gmail.com>
2026-02-06 09:44:53 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Add event listener on multiple elements
+const addEventOnElements = function(elements, eventType, callback){
+    for (let i = 0, len = elements.length; i < len;  i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+// Mobile navbar toggle
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
+
+
+// Active header when window scrolls down to 100px
+
+const header = document.querySelector("[data-header]");
+const headerActive = function(){
+    if(window.scrollY > 100){
+        header.classList.add("active");
+    }else{
+        header.classList.remove("active");
+    }
+}
+
+window.addEventListener("scroll", headerActive);
